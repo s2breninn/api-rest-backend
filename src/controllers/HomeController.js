53e0a3c23c782fef4fc00c1030c39c83e98ea8f6
s2breninn <b.mendesbernardo2020@gmail.com>
2023-10-02 23:32:36 -0300

    Banco de dados  pelo sequlize criados

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,12 +3,12 @@ import Aluno from '../models/Aluno'
 class HomeController{
   async index(req, res) {
     const novoAluno = await Aluno.create({
-      nome: 'Breno',
-      sobrenome: 'Mendes',
-      email: 'b.mendesbernardo2020@gmail.com',
-      idade: 19,
-      peso: 57,
-      altura: 2,
+      nome: 'Maria Clara',
+      sobrenome: 'Mendes Bernardo',
+      email: 'm.mendesbernardo2020@gmail.com',
+      idade: 16,
+      peso: 55,
+      altura: 1.68,
     })
     res.json(novoAluno)
   }
