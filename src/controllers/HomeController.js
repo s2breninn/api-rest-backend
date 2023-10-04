23e89dd237191142d1164f2d4be8b75faa0b0a6e
s2breninn <b.mendesbernardo2020@gmail.com>
2023-10-03 23:58:34 -0300

    Criação de CRUD alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno'
-
 class HomeController{
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Maria Clara',
-      sobrenome: 'Mendes Bernardo',
-      email: 'm.mendesbernardo2020@gmail.com',
-      idade: 16,
-      peso: 55,
-      altura: 1.68,
-    })
-    res.json(novoAluno)
+    res.json('Index')
   }
 }
 
